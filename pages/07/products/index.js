be7c0f6c8b8cca923cffa04ce96eb17ba724da19
IMHEEJAE <imheejae95@gmail.com>
2023-02-24 21:39:26 +0900

    07-04 작업중.

---
+++ pages/07/products/index.js
@@ -0,0 +1,58 @@
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { useState } from "react";
+import styled from "@emotion/styled";
+
+const FETCH_PRODUCTS = gql`
+  query fetchProducts($page: Int) {
+    fetchProducts(page: $page) {
+      _id
+      seller
+      name
+      detail
+      price
+      createdAt
+    }
+  }
+`;
+const DELETE_PRODUCT = gql`
+  mutation ($productId: ID) {
+    deleteProduct(productId: $productId) {
+      message
+    }
+  }
+`;
+const Row = styled.div`
+  display: flex;
+`;
+const Column = styled.div`
+  width: 25%;
+`;
+export default function Page07() {
+  const [deleteProduct] = useMutation(DELETE_PRODUCT);
+  const { data } = useQuery(FETCH_PRODUCTS);
+  console.log(data?.fetchProducts);
+  const onClickDelete = async (event) => {
+    await deleteProduct({
+      variables: { productId: event.target._id },
+      refetchQueries: [{ query: FETCH_PRODUCTS }],
+    });
+  };
+  return (
+    <>
+      {data?.fetchProducts.map((el) => {
+        return (
+          <Row key={el._id}>
+            <Column>{el.seller}</Column>
+            <Column>{el.name}</Column>
+            <Column>{el.detail}</Column>
+            <Column>
+              <button id={el._id} onClick={onClickDelete}>
+                삭제
+              </button>
+            </Column>
+          </Row>
+        );
+      })}
+    </>
+  );
+}
