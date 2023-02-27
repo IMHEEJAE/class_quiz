36d4ff856b329121bec4e361555101ceabce75be
IMHEEJAE <imheejae95@gmail.com>
2023-02-27 16:40:09 +0900

    삭제(완)

--- pages/07/products/index.js
+++ pages/07/products/index.js
@@ -32,27 +32,25 @@ export default function Page07() {
   const { data } = useQuery(FETCH_PRODUCTS);
   const onClickDelete = async (event) => {
     await deleteProduct({
-      variables: { productId: event._id },
+      variables: { productId: event.target.id },
       refetchQueries: [{ query: FETCH_PRODUCTS }],
     });
-    console.log(event.target._id);
+    console.log(event.target.id);
   };
   return (
     <>
-      {data?.fetchProducts.map((el) => {
-        return (
-          <Row key={el._id}>
-            <Column>{el.seller}</Column>
-            <Column>{el.name}</Column>
-            <Column>{el.detail}</Column>
-            <Column>
-              <button id={el._id} onClick={onClickDelete}>
-                삭제
-              </button>
-            </Column>
-          </Row>
-        );
-      })}
+      {data?.fetchProducts.map((el) => (
+        <Row key={el._id}>
+          <Column>{el.seller}</Column>
+          <Column>{el.name}</Column>
+          <Column>{el.detail}</Column>
+          <Column>
+            <button id={el._id} onClick={onClickDelete}>
+              삭제
+            </button>
+          </Column>
+        </Row>
+      ))}
     </>
   );
 }
