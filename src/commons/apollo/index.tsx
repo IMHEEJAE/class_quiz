966eb61fb3b0d2f09d45f08fb3c3cadb1c9c7188
IMHEEJAE <imheejae95@gmail.com>
2023-03-27 17:51:31 +0900

    graphql 주소 변경

--- src/commons/apollo/index.tsx
+++ src/commons/apollo/index.tsx
@@ -1,7 +1,7 @@
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 export default function ApolloSetting(props) {
   const client = new ApolloClient({
-    uri: "http://practice.codebootcamp.co.kr/graphql",
+    uri: "http://backendonline.codebootcamp.co.kr/graphql",
     cache: new InMemoryCache(),
   });
   return (
